16e2e1b569331153db1f4901725770e2ec735de0
Johnathan <jspoliv@gmail.com>
2024-12-05 17:42:39 -0300

    deleted obsolete code

--- assets/js/rnd.js
+++
@@ -1,8 +0,0 @@
-function rnd_bound(e) {
-  let id = e.getAttribute("name");
-  let el = document.getElementById(id);
-  let rnd_val = Math.ceil(Math.random() * el.max);
-  rnd_val = el.value == rnd_val ? rnd_bound(e) : rnd_val;
-  el.value = el.min > rnd_val ? el.min : rnd_val;
-  return rnd_val;
-}
